1038f7570b59d6d78dc023bbd077a04fe4f2a391
victor <vr359490@gmail.com>
2025-06-16 14:25:08 -0500

    Initial Commit

---
+++ RecoverEdge/RecoverEdgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecoverEdgeApp.swift
+//  RecoverEdge
+//
+//  Created by Victor Ruan on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct RecoverEdgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
